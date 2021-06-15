c7aeecfe57cf0f943193fabd6bdff36a91b27caa
Victor Medvedik <ashnikel@gmail.com>
2021-06-15 22:35:40 +0300

    Add line numbers

--- grep-lite/src/main.rs
+++ grep-lite/src/main.rs
@@ -5,9 +5,10 @@ Every face, every shop, bedroom window, public-house, and
 dark square is a picture feverishly turned--in search of what?
 It is the same with books. What do we seek through millions of pages?";
 
-    for line in quote.lines() {
+    for (i, line) in quote.lines().enumerate() {
         if line.contains(search_term) {
-            println!("{}", line);
+            let line_num = i + 1;
+            println!("{}: {}", line_num, line);
         }
     }
 }
