1266e1238d22ee96fe3ee79ffe6ed3d53175b303
Victor Medvedik <ashnikel@gmail.com>
2022-06-03 18:12:33 +0300

    ch08-mget

--- ch08-mget/src/main.rs
+++ ch08-mget/src/main.rs
@@ -8,10 +8,11 @@ mod http;
 
 fn main() {
     let app = App::new("mget")
-        .about ("GET a webpage, manually")
+        .about("GET a webpage, manually")
         .arg(Arg::with_name("url").required(true))
         .arg(Arg::with_name("tap-device").required(true))
-        .arg(Arg::with_name("dns-server").default_value("1.1.1.1")).get_matches();
+        .arg(Arg::with_name("dns-server").default_value("1.1.1.1"))
+        .get_matches();
 
     let url_text = app.value_of("url").unwrap();
     let dns_server_text = app.value_of("dns-server").unwrap();
@@ -19,16 +20,17 @@ fn main() {
 
     let url = Url::parse(url_text).expect("error: unable to parse <url> as  a URL");
     if url.scheme() != "http" {
-        eprintln! ("error: only HTTP protocol supported");
+        eprintln!("error: only HTTP protocol supported");
         return;
     }
 
-    let tap = TapInterface::new(&tap_text)
+    let tap = TapInterface::new(tap_text)
         .expect("error: unable to use <tap-device> as a network interface");
 
     let domain_name = url.host_str().expect("domain name required");
 
-    let _dns_server: std::net::Ipv4Addr = dns_server_text.parse()
+    let _dns_server: std::net::Ipv4Addr = dns_server_text
+        .parse()
         .expect("error: unable to parse <dns-server> as an IPv4 address");
 
     let addr = dns::resolve(dns_server_text, domain_name).unwrap().unwrap();
